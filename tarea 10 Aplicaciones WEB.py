34fb9aee81a2f9130c7993c5bdfd7df28be85899
MarceloTester689 <123722955+MarceloTester689@users.noreply.github.com>
2023-05-05 20:19:45 -0600

    Task 10

---
+++ tarea 10 Aplicaciones WEB.py
@@ -0,0 +1,22 @@
+chuck: [ 
+    { 
+        "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+        "value": "Chuck Norris can skydive into outer space." 
+    }, 
+    { 
+        "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+        "value": "The chief export of Chuck Norris is pain." 
+    }, 
+    { 
+        "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+        "value": "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." 
+    }, 
+    { 
+        "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+        "value": "Time waits for no man. Unless that man is Chuck Norris." 
+    }, 
+    { 
+        "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png", 
+        "value": "If you spell Chuck Norris in Scrabble, you win. Forever." 
+    }, 
+], 
